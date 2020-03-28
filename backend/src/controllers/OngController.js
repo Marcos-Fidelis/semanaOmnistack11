ccb783446dc5ee882004c0ef0ca421bd56776dc8
Locodra <48814062+Marcos-Fidelis@users.noreply.github.com>
2020-03-28 11:38:22 -0300

    quase fim da aula 3

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,5 @@
 const express = require('express'); 
+const crypto = require('crypto');
 
 const OngController = require('../database/connection');
 
@@ -16,19 +17,19 @@ module.exports = {
     async create(request, response) {
         const {name, email, whatsapp, city, uf} = request.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+        const id = crypto.randomBytes(4).toString('HEX');
 
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    })
+      await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        });
 
     
-    return response.json({ id });
+        return response.json({ id });
 
     }
 }
